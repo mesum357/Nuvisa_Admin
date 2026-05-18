76ba5267da5ccd17c06a7b81d3108b6b53392948
Daniyar <adonisdev313@gmail.com>
2026-05-18 23:32:33 +0300

    feat: add feedback submissions page and API endpoint; include feedback fetching logic

--- src/app/(admin)/admin/general-content/page.tsx
+++ src/app/(admin)/admin/general-content/page.tsx
@@ -27,7 +27,7 @@ export default function GeneralContentPage() {
       <ContactContentForm />
       <UrgentContentForm />
       <MoreToLoveContentForm />
-      {/* <PriceContentForm /> */}
+      <PriceContentForm />
     </div>
   );
 }
